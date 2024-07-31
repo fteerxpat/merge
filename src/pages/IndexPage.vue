7d6e27d00faa01e802653cb20c69c99c5f5c22b1
fteerxpat <fluketeerapatlearning@gmail.com>
2024-07-31 10:40:35 +0700

    modified id-code

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -6,6 +6,15 @@
       @reset="onReset"
       class="q-gutter-md"
     >
+    <!--id-->
+    <q-input
+        filled
+        v-model="id"
+        label="Your id *"
+        hint="id-code"
+        lazy-rules
+        :rules="[ val => val && val.length > 0 || 'Please type your id-code']"
+      />
       <!--name-->
       <q-input
         filled
@@ -55,13 +64,14 @@ import { ref } from 'vue'
 export default {
   setup () {
     const $q = useQuasar()
-
     const name = ref("ธีรภัทร")
+    const id = ref("6604101339")
     const surename = ref("พอจิต")
     const age = ref(null)
     const accept = ref(false)
 
     return {
+      id,
       name,
       surename,
       age,
